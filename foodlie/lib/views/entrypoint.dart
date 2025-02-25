035140828cd1d596a7a323e469d02d38cb0e48bd
NanseraEna <enanansera311@gmail.com>
2025-02-25 18:11:21 +0300

    small changes

--- foodlie/lib/views/entrypoint.dart
+++ foodlie/lib/views/entrypoint.dart
@@ -3,55 +3,64 @@ import 'package:foodlie/constants/constants.dart';
 import 'package:get/get.dart';
 import 'package:foodlie/controllers/tab_index_controller.dart'; // Add this line to import the Controller class
 // Make sure this import is correct
-
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 class MainScreen extends StatelessWidget {
-  MainScreen({Key? key}) : super(key: key);
+MainScreen({Key? key}) : super(key: key);
 
-  final controller = Get.put(TabIndexController()); // Define correctly
+final controller = Get.put(TabIndexController()); // Define correctly
 
-  @override
-  Widget build(BuildContext context) {
-    final double height = MediaQuery.of(context).size.height;
-    final double width = MediaQuery.of(context).size.width;
+@override
+Widget build(BuildContext context) {
+  final double height = MediaQuery.of(context).size.height;
+  final double width = MediaQuery.of(context).size.width;
 
-    return Obx(() => Scaffold(
-          body: Stack(
-            children: [
-              Container(
-                height: height,
-                width: width,
-                color: kPrimary,
-              ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Theme(
-                  data: Theme.of(context).copyWith(canvasColor: kPrimary),
-                  child: BottomNavigationBar(
-                    showSelectedLabels: false,
-                    showUnselectedLabels: false,
-                    unselectedIconTheme:
-                        const IconThemeData(color: Colors.black38),
-                    selectedIconTheme: const IconThemeData(color: kSecondary),
-                    onTap: (value) {
-                      controller.setTabIndex(value);
-                    },
-                    currentIndex: controller.tabIndex.value,
-                    items: const [
-                      BottomNavigationBarItem(
-                          icon: Icon(Icons.home), label: 'Home'),
-                      BottomNavigationBarItem(
-                          icon: Icon(Icons.search), label: 'Search'),
-                      BottomNavigationBarItem(
-                          icon: Icon(Icons.shopping_cart), label: 'cart'),
-                      BottomNavigationBarItem(
-                          icon: Icon(Icons.person), label: 'Profile'),
-                    ],
-                  ),
+  return Obx(() => Scaffold(
+        body: Stack(
+          children: [
+            Container(
+              height: height,
+              width: width,
+              color: kPrimary,
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Theme(
+                data: Theme.of(context).copyWith(canvasColor: kPrimary),
+                child: BottomNavigationBar(
+                  showSelectedLabels: false,
+                  showUnselectedLabels: false,
+                  unselectedIconTheme:
+                      const IconThemeData(color: Colors.black38),
+                  selectedIconTheme: const IconThemeData(color: kSecondary),
+                  onTap: (value) {
+                    controller.setTabIndex(value);
+                  },
+                  currentIndex: controller.tabIndex.value,
+                  items: [
+                    BottomNavigationBarItem(
+                    // ignore: unrelated_type_equality_checks
+                    icon: controller.tabIndex ==0 ?
+                     const Icon(AntDesign.apple1) :
+                      const Icon(AntDesign.apple_o), label: 'Home'),
+                    const BottomNavigationBarItem(
+                        icon: Icon(Icons.search), label: 'Search'),
+                    const BottomNavigationBarItem(icon: 
+                    Badge(
+                      label: Text("1"),
+                      child: Icon(FontAwesome.opencart),
+                    ), label: 'cart'),
+                    BottomNavigationBarItem(
+                        // ignore: unrelated_type_equality_checks
+                        icon: controller.tabIndex ==3 
+                        ?const Icon(FontAwesome.user_circle) 
+                        :const Icon(FontAwesome.user_circle_o), label: 'Profile'),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ));
-  }
+            ),
+          ],
+        ),
+      ));
+}
 }
 
